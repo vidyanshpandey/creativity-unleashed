f6388e7490e20556ce6c409a9ee380e3c631e794
creativity-unleashed <vidyanshpandey540@gmail.com>
2024-12-25 17:23:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+ console.log("fjhfjdshkfdfkgnfdkg")
+  document.querySelector('.cross').style.display ='none'
+ document.querySelector('.hamburger').addEventListener("click",()=>{
+     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+ })
