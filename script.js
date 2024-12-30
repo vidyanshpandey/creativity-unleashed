f1130297b5e4840286a48d2fa4bb49ea1123e071
Vidyansh Pandey <vidyanshpandey540@gmail.com>
2024-12-30 20:29:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
- console.log("fjhfjdshkfdfkgnfdkg")
-  document.querySelector('.cross').style.display ='none'
- document.querySelector('.hamburger').addEventListener("click",()=>{
-     document.querySelector('.sidebar').classList.toggle('sidebarGo');
- })
